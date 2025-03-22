6374479b667df652efe2eb6dfd609dfc47260a23
Antoine Morrier <antoine.morrier@gmail.com>
2025-03-22 17:44:00 +0100

    Genericize Vertex

--- VulkanWrapper/include/VulkanWrapper/Descriptors/Vertex.h
+++ VulkanWrapper/include/VulkanWrapper/Descriptors/Vertex.h
@@ -1,52 +1,85 @@
 #pragma once
 
 namespace vw {
-struct ColoredVertex2D {
-    glm::vec2 vertex;
-    glm::vec3 color;
 
+template <typename T> struct format_from;
+
+template <>
+struct format_from<float>
+    : std::integral_constant<vk::Format, vk::Format::eR32Sfloat> {};
+
+template <>
+struct format_from<glm::vec2>
+    : std::integral_constant<vk::Format, vk::Format::eR32G32Sfloat> {};
+
+template <>
+struct format_from<glm::vec3>
+    : std::integral_constant<vk::Format, vk::Format::eR32G32B32Sfloat> {};
+
+template <>
+struct format_from<glm::vec4>
+    : std::integral_constant<vk::Format, vk::Format::eR32G32B32A32Sfloat> {};
+
+template <typename... Ts> class VertexInterface {
+  public:
     [[nodiscard]] static constexpr auto binding_description(int binding) {
-        return vk::VertexInputBindingDescription(
-            binding, sizeof(ColoredVertex2D), vk::VertexInputRate::eVertex);
+        return vk::VertexInputBindingDescription(binding, (sizeof(Ts) + ...),
+                                                 vk::VertexInputRate::eVertex);
     }
 
     [[nodiscard]] static constexpr auto attribute_descriptions(int binding,
                                                                int location) {
-        return std::array{vk::VertexInputAttributeDescription(
-                              location + 0, binding, vk::Format::eR32G32Sfloat,
-                              offsetof(ColoredVertex2D, vertex)),
-                          vk::VertexInputAttributeDescription(
-                              location + 1, binding,
-                              vk::Format::eR32G32B32Sfloat,
-                              offsetof(ColoredVertex2D, color))};
+        constexpr auto offsets = []() {
+            auto sizes = std::array{sizeof(Ts)...};
+            std::exclusive_scan(begin(sizes), end(sizes), begin(sizes), 0);
+            return sizes;
+        }();
+
+        std::array attributes = {
+            vk::VertexInputAttributeDescription().setFormat(
+                format_from<Ts>::value)...};
+
+        for (int i = 0; i < sizeof...(Ts); ++i) {
+            attributes[i].location = location + i;
+            attributes[i].binding = binding;
+            attributes[i].offset = offsets[i];
+        }
+
+        return attributes;
     }
 };
 
-struct ColoredAndTexturedVertex2D {
-    glm::vec2 vertex;
-    glm::vec3 color;
-    glm::vec2 texCoord;
+template <typename Position>
+struct ColoredVertex : VertexInterface<Position, glm::vec3> {
+    ColoredVertex() noexcept = default;
+    ColoredVertex(Position position, glm::vec3 color) noexcept
+        : position{position}
+        , color{color} {}
 
-    [[nodiscard]] static constexpr auto binding_description(int binding) {
-        return vk::VertexInputBindingDescription(
-            binding, sizeof(ColoredAndTexturedVertex2D),
-            vk::VertexInputRate::eVertex);
-    }
-
-    [[nodiscard]] static constexpr auto attribute_descriptions(int binding,
-                                                               int location) {
-        return std::array{vk::VertexInputAttributeDescription(
-                              location + 0, binding, vk::Format::eR32G32Sfloat,
-                              offsetof(ColoredAndTexturedVertex2D, vertex)),
-                          vk::VertexInputAttributeDescription(
-                              location + 1, binding,
-                              vk::Format::eR32G32B32Sfloat,
-                              offsetof(ColoredAndTexturedVertex2D, color)),
-                          vk::VertexInputAttributeDescription(
-                              location + 2, binding, vk::Format::eR32G32Sfloat,
-                              offsetof(ColoredAndTexturedVertex2D, texCoord))};
-    }
+    Position position{};
+    glm::vec3 color{};
 };
+
+template <typename Position>
+struct ColoredAndTexturedVertex
+    : VertexInterface<Position, glm::vec3, glm::vec2> {
+    ColoredAndTexturedVertex() noexcept = default;
+    ColoredAndTexturedVertex(Position position, glm::vec3 color,
+                             glm::vec2 texCoord) noexcept
+        : position{position}
+        , color{color}
+        , texCoord{texCoord} {}
+
+    Position position{};
+    glm::vec3 color{};
+    glm::vec2 texCoord{};
+};
+
+using ColoredVertex2D = ColoredVertex<glm::vec2>;
+using ColoredAndTexturedVertex2D = ColoredAndTexturedVertex<glm::vec2>;
+
+using ColoredVertex3D = ColoredVertex<glm::vec3>;
+using ColoredAndTexturedVertex3D = ColoredAndTexturedVertex<glm::vec3>;
 
 template <typename T>
 concept Vertex =
