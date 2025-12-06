355931d7860ebfa911509aed9ad561f83e8698b3
Antoine Morrier <antoine.morrier@gmail.com>
2025-12-06 13:34:38 +0100

    Add Application example base class
    - Add Application: Common example framework
      - Window and Vulkan initialization
      - Main loop with frame timing
      - Input handling via SDL events
      - Swapchain recreation on resize
    - Add basic example demonstrating:
      - Triangle rendering
      - Shader loading
      - Basic pipeline setup
    - Provides foundation for advanced examples

---
+++ examples/Application/Application.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include "VulkanWrapper/3rd_party.h"
+#include "VulkanWrapper/Memory/Allocator.h"
+#include "VulkanWrapper/Vulkan/Device.h"
+#include "VulkanWrapper/Vulkan/DeviceFinder.h"
+#include "VulkanWrapper/Vulkan/Instance.h"
+#include "VulkanWrapper/Vulkan/Surface.h"
+#include "VulkanWrapper/Vulkan/Swapchain.h"
+#include "VulkanWrapper/Window/SDL_Initializer.h"
+#include "VulkanWrapper/Window/Window.h"
+#include <memory>
+
+class App {
+  public:
+    App() = default;
+
+  public:
+    std::shared_ptr<vw::SDL_Initializer> initializer = std::make_shared<vw::SDL_Initializer>();
+    vw::Window window = vw::WindowBuilder(initializer)
+                            .with_title("Vulkan Wrapper")
+                            .sized(vw::Width(800), vw::Height(600))
+                            .build();
+
+    std::shared_ptr<vw::Instance> instance =
+        vw::InstanceBuilder()
+            .addPortability()
+            .addExtensions(window.get_required_instance_extensions())
+            .setApiVersion(vw::ApiVersion::e13)
+            .build();
+
+    vw::Surface surface = window.create_surface(*instance);
+
+    std::shared_ptr<vw::Device> device =
+        instance->findGpu()
+            .with_queue(vk::QueueFlagBits::eGraphics |
+                        vk::QueueFlagBits::eCompute |
+                        vk::QueueFlagBits::eTransfer)
+            .with_presentation(surface.handle())
+            .with_synchronization_2()
+            .with_ray_tracing()
+            .with_dynamic_rendering()
+            .build();
+
+    std::shared_ptr<vw::Allocator> allocator =
+        vw::AllocatorBuilder(instance, device).build();
+
+    vw::Swapchain swapchain =
+        window.create_swapchain(device, surface.handle());
+};
